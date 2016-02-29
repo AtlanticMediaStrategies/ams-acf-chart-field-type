2d6e126872126a7d88435534e028b8f371533f43
Russell Vea <rvea@atlanticmedia.com>
2016-02-29 12:15:57 -1000

    Pulling in react-dropzone
    User should now see a place to drag/drop to import graph.
    
    [Finishes #114723249]

--- assets/src/containers/Home/index.js
+++ assets/src/containers/Home/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import DocumentMeta from 'react-document-meta';
+import Dropzone from 'react-dropzone';
+import styles from './home.scss';
 
 /* components */
 
 export class Home extends Component {
+  handleFiles(files) {
+    console.log(files)
+  }
   render() {
     return (
       <section>
-        <h1>Hello, World</h1>
+        <Dropzone onDrop={this.handleFiles.bind(this)} accept="text/csv">
+          <div className="drop-text">Drop csv files here or click to trigger form.</div>
+        </Dropzone>
       </section>
     );
   }
