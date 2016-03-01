20fed75aeca2de9bb7f03e7fb6692c0a434ab388
Russell Vea <rvea@atlanticmedia.com>
2016-02-29 18:00:25 -1000

    Simple tests
    Just instantiating the plugin for now.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,5 +10,11 @@ global $wp_rewrite, $wpdb;
 require_once( dirname( __FILE__ ) . '/../../../../wp-load.php' );
 require_once( ABSPATH . 'wp-admin/includes/admin.php' );
 
+function require_chart_field() {
+  require_once dirname(dirname(__FILE)) . '/fields/acf-chart-v5.php';
+}
+
+add_filter('muplugins_loaded', 'require_chart_field');
+
 wp_set_current_user( 1 );
 ?>
--- tests/test-acf-class.php
+++ tests/test-acf-class.php
@@ -1,7 +1,9 @@
 <?php
+  global $wpdb;
   class ACF_Field_Test extends PHPUnit_Framework_TestCase {
     public function test_acf_class() {
-      $this->assertEquals(1, 1);
+      $klass = new acf_field_chart();
+      $this->assertEquals($klass->name, 'chart');
     }
   }
 ?>
