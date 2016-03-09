bc77bdfc189532bbc8d7c5106adee6722a95337f
Russell Vea <rvea@atlanticmedia.com>
2016-03-08 14:53:06 -1000

    Misc changes
    * Removing boilerplate sass files
    * Installing react-a11y

--- assets/src/containers/App/index.js
+++ assets/src/containers/App/index.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
-/* global styles for app */
-import './styles/app.scss';
-
 /* application components */
-
 export class App extends Component {
   static propTypes = {
     children: React.PropTypes.any,
--- assets/src/index.js
+++ assets/src/index.js
@@ -5,8 +5,13 @@ import { Router, useRouterHistor, browserHistory} from 'react-router';
 import { syncHistoryWithStore} from 'react-router-redux';
 import configureStore from './store/configureStore';
 import routes from './routes';
-const store = configureStore({}, history);
+const store = configureStore({});
 const history = syncHistoryWithStore(browserHistory, store);
+import a11y from 'react-a11y';
+
+if(process.env.NODE_ENV !== 'production') {
+  a11y(React);
+}
 require('es6-shim')
 
 const apps = document.querySelectorAll('.acf-chart');
