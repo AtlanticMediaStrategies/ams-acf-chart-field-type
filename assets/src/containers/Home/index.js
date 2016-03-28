a6a2b10ed8fa805ff4914fe6454f13ad00615987
Russell Vea <rvea@atlanticmedia.com>
2016-03-28 12:48:14 -1000

    Adding ARIA labels, fixing warnings in console

--- assets/src/containers/Home/index.js
+++ assets/src/containers/Home/index.js
@@ -217,24 +217,30 @@ export class Home extends Component {
           <h3 for="pie">Chart Type</h3>
           <form onChange={this.set_type.bind(this)}>
             <Radio
-                type="radio"
-                checked={type == 'pie'}
-                label="Pie Chart"
-                name="pie"
-                value="pie"
-                group="type"
+              type="radio"
+              checked={type == 'pie'}
+              label="Pie Chart"
+              aria-label="Pie Chart"
+              name="pie"
+              value="pie"
+              readOnly={true}
+              group="type"
             >
             </Radio>
             <Radio
               label="Line Chart"
+              aria-label="Line Chart"
               name="line"
+              readOnly={true}
               checked={type == 'line'}
               value="line"
             >
             </Radio>
             <Radio
+              aria-label="Bar Chart"
               label="Bar Chart"
               name="bar"
+              readOnly={true}
               checked={type == 'bar'}
               value="bar"
             >
